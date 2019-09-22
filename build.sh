77810c072b05c0ea1bec775760f38e507dc869ab
Carlos Zambrana-Torrelio <cmzambranat@gmail.com>
2019-09-22 10:19:47 -0400

    fix  build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@
 set -e 
 set -x
 
-for tag in apt rcppeigen inla rpkg server
+for tag in apt rcppeigen rstan inla rpkg server gpu
 do
-  image=cmzambranat/sajama:$tag
-  #time docker pull $image
-  time docker build -q -f Dockerfile.$tag --cache-from $image -t $image .
+  image=ecohealthalliance/reservoir:$tag
+  docker build -q -f Dockerfile.$tag --cache-from $image -t $image .
 done
