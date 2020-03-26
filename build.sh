d06316f1c1a2f4eeb543e48498b0fa556533e61b
Carlos Zambrana-Torrelio <cmzambranat@gmail.com>
2020-03-26 11:21:24 -0400

    update docker config

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -x
 
 for tag in apt rcppeigen inla rpkg server
 do
-  image=mzambranat/sajama:$tag
+  image=cmzambranat/sajama:$tag
   time docker build -f Dockerfile.$tag --cache-from $image -t $image .
 done
